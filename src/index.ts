64c6979a1727dd1e105fc8eb0aaf691c21fce03f
Bailig <barrybailig@hotmail.com>
2023-02-24 16:17:59 +0300

    added fetches

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,18 @@ import type { ZodRawShape, ZodTypeAny } from "zod";
 
 type Fetch = typeof fetch;
 
-const get = async <Schema extends ZodTypeAny>(
+type Get = <Schema extends z.ZodTypeAny>(
   url: Parameters<Fetch>[0],
   schema: Schema,
   options?: Parameters<Fetch>[1]
-): Promise<z.infer<Schema>> => {
-  const response = await fetch(url, options);
-  return schema.parse(await response.json());
-};
+) => Promise<z.infer<Schema>>;
 
-const post = async <Schema extends ZodTypeAny>(
+type Post = <Schema extends z.ZodTypeAny>(
   url: Parameters<Fetch>[0],
   schema: Schema,
   data?: any,
   options?: Parameters<Fetch>[1]
-): Promise<z.infer<Schema>> => {
-  const response = await fetch(url, { ...options, body: JSON.stringify(data) });
-  return schema.parse(await response.json());
-};
+) => Promise<z.TypeOf<Schema>>;
 
 export class FetcherClient<
   TContextSchema extends ZodTypeAny,
@@ -31,8 +25,8 @@ export class FetcherClient<
       options: {
         ctx: z.infer<TContextSchema>;
         headers: InferZodRawShape<THeadersShape>;
-        get: typeof get;
-        post: typeof post;
+        get: Get;
+        post: Post;
       },
       input: any
     ) => any
@@ -52,6 +46,40 @@ export class FetcherClient<
     this.fetchers = options.fetchers;
   }
 
+  private createGet = (headers: InferZodRawShape<THeadersShape>): Get => {
+    const get: Get = async (url, schema, options) => {
+      const response = await fetch(url, {
+        ...options,
+        headers: {
+          ...headers,
+          ...options?.headers,
+        },
+      });
+      return schema.parse(await response.json());
+    };
+
+    return get;
+  };
+
+  private createPost = (headers: InferZodRawShape<THeadersShape>): Post => {
+    const post: Post = async (url, schema, data, options) => {
+      const response = await fetch(url, {
+        method: "POST",
+        cache: "no-cache",
+        referrerPolicy: "no-referrer",
+        body: JSON.stringify(data),
+        ...options,
+        headers: {
+          "Content-Type": "application/json; charset=UTF-8",
+          ...headers,
+          ...options?.headers,
+        },
+      });
+      return schema.parse(await response.json());
+    };
+    return post;
+  };
+
   createFetcher = (options: {
     ctx: z.infer<TContextSchema>;
     headers: InferZodRawShape<THeadersShape>;
@@ -59,6 +87,8 @@ export class FetcherClient<
     const ctx = this.ctxSchema.parse(options.ctx);
     const headerSchema = z.object(this.headersShape);
     const headers = headerSchema.parse(options.headers) as any;
+    const get = this.createGet(headers);
+    const post = this.createPost(headers);
 
     const fetchers = Object.entries(this.fetchers).reduce(
       (acc, [key, fetcher]) => {
